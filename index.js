90da7fb0fa3d0196e0ad046e915d74afd246dae2
Kristin <kristinponsonby@gmail.com>
2021-08-30 09:35:11 -0400

    commit tree 81cc6da3a9d4b16eb18ce6e56c6224c156aadc60

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-function mapToNegativize(src) {
-    let r = []
-    for (let i = 0; i < src.length; i++ ) {
-      r.push(-1 * src[i]) // Unique work
+function map(src, callback) {
+    const newArray = [];
+  
+    for (let i = 0; i < src.length; i++) {
+      const theElement = src[i];
+      newArray.push(callback(theElement));
     }
-    return r
+  
+    return newArray;
   }
 
-
-function map(src), mapToNegativize(src) {
-    return mapToNegativize
-}
+  function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    }
+  }
