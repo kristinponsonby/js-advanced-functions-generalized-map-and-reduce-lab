f8bacd154ba24a172e0af908312b0a7a3c837d82
Kristin <kristinponsonby@gmail.com>
2021-08-30 09:00:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+function mapToNegativize(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(-1 * src[i]) // Unique work
+    }
+    return r
+  }
+
+
+function map(mapToNegativize) {
+    return mapToNegativize
+}
